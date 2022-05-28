cb388d00eff79e2dc5b004bfc0ff9dddc00de1d1
Demon <74318897+D3M0N99@users.noreply.github.com>
2022-05-28 14:13:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ import os
 class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
+ SOURCE="https://github.com/D3M0N99/ForwardTagRemoverBot"
  START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©D3M0N_xD"
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
